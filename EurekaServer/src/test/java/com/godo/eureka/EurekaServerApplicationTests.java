ce5adfa1cc5aaceb28f3c6dd3b88583b2f5ad76e
JeongSeonggil <sgjeong1108@gmail.com>
2022-11-10 13:50:15 +0900

    :tada: Initial commit

---
+++ EurekaServer/src/test/java/com/godo/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godo.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ TrainerService/src/test/java/com/gogo/trainer/TrainerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gogo.trainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
